41c5d5e67a6deb75881906241dc2ec1143fa5e0f
Guido Giuntoli <gagiuntoli@gmail.com>
2023-07-12 10:01:43 +0200

    Re-export errors

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 pub mod elliptic_curve;
 pub mod finite_fields;
 
-pub use elliptic_curve::{EllipticCurve, Point};
-pub use finite_fields::FiniteField;
+pub use elliptic_curve::{EllipticCurve, EllipticCurveError, Point};
+pub use finite_fields::{FiniteField, FiniteFieldError};
